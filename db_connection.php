decd4ea5072c97da8547dfdde05d803c0da7bbe8
Raneen AlRammahi <raneenhaider4@gmail.com>
2026-03-24 16:56:52 -0400

    database connected

---
+++ db_connection.php
@@ -0,0 +1,6 @@
+<?php
+ $con = mysqli_connect("localhost","root","root")or 
+     die("Connection failed: " . mysqli_connect_error());
+     mysqli_set_charset($con,"utf8mb4");
+     $db = mysqli_select_db($con,"course_project") or
+     die("Could not select database: " . mysqli_error($con));
